58b59996cd8a50633bb72aacad70dd1a6b15d484
Craig Carpenter <carpenlc@localhost.localdomain>
2016-10-14 10:21:48 -0400

    fixed define statements

--- include/ec2_metadata.h
+++ include/ec2_metadata.h
@@ -30,39 +30,39 @@ typedef struct {
 /*
  * Base URL for the EC2 instance metadata.
  */
-#define EC2_INSTANCE_META_DATA_URL "http://instance-data/latest/meta-data/";
+#define EC2_INSTANCE_META_DATA_URL "http://instance-data/latest/meta-data/"
 
 /*
  * Offset URL context for retrieving the temporary IAM role credentials.
  */
-#define EC2_IAM_ROLE_OFFSET "iam/security-credentials/";
+#define EC2_IAM_ROLE_OFFSET "iam/security-credentials/"
 
 /*
  * JSON element containing the time when the IAM role credentials were last
  * updated.
  */
-#define JSON_AWS_CREDENTIAL_CREATED "LastUpdated";
+#define JSON_AWS_CREDENTIAL_CREATED "LastUpdated"
 
 /*
  * JSON element containing the time when the IAM role credentials are set to
  * expire.
  */
-#define JSON_AWS_CREDENTIAL_EXPIRATION "Expiration";
+#define JSON_AWS_CREDENTIAL_EXPIRATION "Expiration"
 
 /*
  * JSON element containing the AWS access key.
  */
-#define JSON_AWS_ACCESS_KEY "AccessKeyId";
+#define JSON_AWS_ACCESS_KEY "AccessKeyId"
 
 /*
  * JSON element containing the AWS secret access key.
  */
-#define JSON_AWS_SECRET_ACCESS_KEY "SecretAccessKey";
+#define JSON_AWS_SECRET_ACCESS_KEY "SecretAccessKey"
 
 /*
  * JSON element containing the token
  */
-#define JSON_AWS_TOKEN "Token";
+#define JSON_AWS_TOKEN "Token"
 
 /*
  * The number of tokens (i.e. tokens in the context of the jsmn JSON
@@ -70,21 +70,21 @@ typedef struct {
  * actually only 14 tokens, however, the squirrelly jsmn JSON processor
  * requires one extra to handle the trailing bracket.)
  */
-#define JSON_AWS_TOKEN_COUNT 15;
+#define JSON_AWS_TOKEN_COUNT 15
 
 /*
  * New credentials should be made available when we are within 5 minutes
  * of expiration.  Setting this value to less than 5 should give us a couple
  * of chances to retrieve new credentials.
  */
-#define CREDENTIAL_EXPIRATION_WINDOW (3 * 60);
+#define CREDENTIAL_EXPIRATION_WINDOW (3 * 60)
 
 /*
  * Defines the number of seconds between checks on the expiration status of
  * the AWS credentials.  Since EC2 expires credentials far more frequently
  * than commercial AWS we're checking every minute.
  */
-#define CREDENTIAL_ALARM_DURATION 60;
+#define CREDENTIAL_ALARM_DURATION 60
 
 /*
  * Prototype for function that will determine if we need to get new 
